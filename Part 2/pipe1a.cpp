424ece6f377ad4ca1e6d53ed5d67cc5dbb6a120b
lmontalvo27 <39773991+lmontalvo27@users.noreply.github.com>
2021-10-16 23:14:08 -0700

    Update pipe1a.cpp

--- Part 2/pipe1a.cpp
+++ Part 2/pipe1a.cpp
@@ -14,7 +14,12 @@ int main(int argc, char *argv[]) //command line argument
 {
   char userIn[100];//user input uninitialized
   
-  strcpy(userIn, argv[1]); //getting the user input
+  strcpy(userIn, argv[1]); //copying first 
+  for(int i = 2; i < argc; i++) 
+    {
+        strcat(userIn, " ");   //to have a whitespace in between words
+        strcat(userIn, argv[i]); //the idea for this loop came from the youtube video https://youtu.be/aP1ijjeZc24 to be able to store possible multiple arguments [1], [2] etc
+    }
 
   FILE *fpi;				//for reading a pipe
  
