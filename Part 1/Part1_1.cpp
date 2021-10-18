7a893d3e731c14fafd50f5d08598a1b2082f8f6c
Zamidation <social_genius3@hotmail.com>
2021-10-17 22:50:28 -0700

    Finishing touches to all the code

--- Part 1/Part1_1.cpp
+++ Part 1/Part1_1.cpp
@@ -12,58 +12,50 @@ using namespace std;
 
 int main(int artgc, char *argv[])
 {
-    pid_t pid; // process ID
+    
+    //pid_t pid; // process ID
     int status;
     char *name = argv[1];
     int n;
-    int exit_code;
+    int counter;
 
+    // Create an array of process IDs, all will be appended and include numbers
+    pid_t pid[11];
+
+    pid[0] = getpid();
+    
     cout << "Part 1 is starting.\n";
+
+    cout << "I am the Parent with process ID: " << pid[0] << endl;
 
     for(int i = 0; i < 10; i++) // create 9 children, 10 processes in total
     {
-        pid = fork(); // Parent will go to default, child will go to 0; hopefully not create 2^10 processes
-        switch (pid)
+        n = fork(); // Parent will go to default, child will go to 0;
+        switch (n)
         {
         case 0: // child process
-            cout << "I am a child with process ID %d\n: "
+            pid[i+1] = getpid();
+            //cout << "\nI am a child with process ID: " << pid[i] << "and my parent ID is: " << pid[i-1] << endl;
+            cout << "\nI am a child with process ID: " << getpid() << " and my Ancestors are: ";
+            counter = i;
+            while(counter != 0)
+            {
+                cout << pid[counter] << " , ";
+                counter -= 1;
+            }
+            cout << pid[0] << endl;
+            //cout << endl;
             break;
-    
-        default:
+        default:    // Parent process
+            //cout << "\nI am the Parent with process ID: " << pid[i] << endl;
+            //pid[i] = getpid();
+            wait(&status); //wait for child, Child finishes here
             break;
         }
+        if(n != 0) // if a parent process, break from for loop
+            //return 0;
+            break;
     }
-    
-}
-
-/*
-int main(int argc, char *argv[]){
-    int status;
-    char *name = argv[1];
-    // create new process using fork. The call to fork in the parent 
-    // return the PID of the new child process
-    pid_t pid;
-    //int pid;
-    for(int i = 0; i < 2; i++)
-    {
-        pid = fork(); // ONLY WORKS WITH 1 PROCESS, need a way to define more processes
-    }
-    if(pid == 0)
-    {
-        // Child process continues to execute, exception that 
-        // child process the call to the fork remains 0
-        printf("Child of %s is %d\n", name, pid);
-        //execlp("/bin/ls", "ls", NULL); // Run exec command for the child to display the content of the current directory
-        return 0;
-    }
-
-    else
-    {
-        printf("I am the parent, my child is %d\n", pid);
-        wait(&status); //wait for child, Child finishes here
-        printf("Exit status: %d\n", WEXITSTATUS(status)); // print the exit status of the child
-    }
-
+        
     return 0;
 }
-*/
--- Part 1/test_wait.cpp
+++ Part 1/test_wait.cpp
@@ -14,29 +14,54 @@ int main()
   char *message;
   int n;
   int exit_code;
- 
+  int status;
+
+  pid_t grandchild[3];
+
+  grandchild[0] = getpid();
+
   cout << "fork program starting\n";
-  pid = fork();
-  switch (pid) {
-  case -1:
-    cout << "Fork failure!\n";
-    return 1;
-  case 0:
-    message = "This is the child\n";
-    n = 5;
-    exit_code = 9;
-    break;
-  default:
-    message = "This is the parent\n";
-    n = 3;
-    exit_code = 0;
-    break;
+  
+  for(int j = 0; j < 2; j++) // do it twice
+  {
+    pid = fork();
+    switch (pid) {
+    case -1:
+      cout << "Fork failure!\n";
+      return 1;
+    case 0: //children
+      grandchild[j+1] = getpid();
+      if (j == 0){
+        message = "This is the child\n";
+        }
+      else
+        message = "This is the grandchild with ID: ";
+      n = 5;
+      exit_code = 9;
+      break;
+    default:
+      message = "This is the parent\n";
+      n = 3;
+      exit_code = 0;
+      break;
+    }
+
+    //if(getpid() != grandchild[3]);
+    //  wait(&status); //wait for child, Child finishes here
+
+    for (int i = 0; i < n; ++i) {
+      
+      if(j == 1 && n == 5) // grandchild
+      {
+        cout << message << grandchild[2] << " and my Ancestors are: " << grandchild[1] << " , " << grandchild[0] << endl;
+      }
+      else {
+        cout << message;}
+      sleep (1);
+    }
+    if(pid != 0) // release the parents 
+      break;
   }
-  for (int i = 0; i < n; ++i) {
-    cout << message;
-    sleep (1);
-  }
- 
   //waiting for child to finish
   if (pid != 0) {        //parent 
     int stat_val;
